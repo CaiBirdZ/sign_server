ed6e5894785ed1194367528e8c6d70a1dc2f8ce0
CaiBirdZ <1195239543@qq.com>
2017-02-15 13:55:00 +0800

    change
    用户模块

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     req.getConnection(function(err,conn){
         if(err){
             return next(err);
         }else{
             console.log(req.body);
-            conn.query("select * from user_info where jobNo = ?,password = ?",[req.body.userNo,req.body.password],function(err,result){
+            conn.query("select * from user_info where jobNo = ?",[req.body.userNo],function(err,result){
                 if(err){
-                    return res.send({code:0});
+                    console.log(err);
+                    return res.send({code:'003'});
                     //next(err);
                 }else {
-                    return res.send({code:1,result:result});
+                    //conn.query("select * from user_info where jobNo = ?,password = ?",[req.body.userNo,req.body.password],function(err,result){
+                    //    if(err){
+                    //        return res.send({code:'002'});
+                    //        //next(err);
+                    //    }else {
+                    //        console.log(result);
+                    //        return res.send({code:'001',result:result});
+                    //    }
+                    //});
+                    if(result[0].password === req.body.password)
+                    {
+                        console.log(result);
+                        return res.send({code:'000',result:result});
+                    }else {
+
+                        return res.send({code:'002'});
+                    }
                 }
             });
         }
