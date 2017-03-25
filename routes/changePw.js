a71cfd9ae369893f4ff951dcabd4677a5f948d85
CaiBirdZ <1195239543@qq.com>
2017-03-25 12:35:06 +0800

    wsa

--- routes/changePw.js
+++ routes/changePw.js
@@ -48,7 +48,7 @@ router.post('/confirm', function(req, res, next) {
         {
             'sms_type':'normal',
             'sms_free_sign_name':'爱签',
-            'sms_param':'{\"emName\":\"张闯\",\"number\":'+idCode.toString()+'}',
+            'sms_param':'{\"emName\":\"张闯\",\"number\":'+'\"'+idCode+'\"'+'}',
             'rec_num':req.body.emTel,
             'sms_template_code':'SMS_53530126'
         },
