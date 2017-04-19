db2c233d06575017bcb4c373e2987bed8cd40224
CaiBirdZ <1195239543@qq.com>
2017-04-19 19:08:05 +0800

    a

--- public/javascripts/queryRecord.js
+++ public/javascripts/queryRecord.js
@@ -102,24 +102,24 @@ function byDepartmentAndSignDate(){
 
 function gethtml()
 {
-    var fTableElement,strcell,rows,cols,style,excelstr;
-    fTableElement = document.getElementById("exportToExel");
+    var rows,cols,style,excelstr;
+
     excelstr = "<html><head><!--<xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>工作表标题</x:Name><x:WorksheetOptions><x:print><x:ValidPrinterInfo /></x:print></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head>";
     excelstr += "<table border='1'>";
-    for (var i = 0; i < fTableElement.rows.length; i++)
+    for (var i = 0; i < $("#exportToExel").rows.length; i++)
     {
         excelstr += "<tr>";
-        for (var  j = 0; j < fTableElement.rows[i].cells.length; j++)
+        for (var  j = 0; j < $("#exportToExel").rows[i].cells.length; j++)
         {
-            rows = fTableElement.rows[i].cells[j].rowSpan;
-            cols = fTableElement.rows[i].cells[j].colSpan;
-            style = fTableElement.rows[i].cells[j].align;
+            rows = $("#exportToExel").rows[i].cells[j].rowSpan;
+            cols = $("#exportToExel").rows[i].cells[j].colSpan;
+            style = $("#exportToExel").rows[i].cells[j].align;
             excelstr += "<td colspan=" + cols + " rowspan=" + rows + " align=" + style + ">";
-            for(var k = 0; k < fTableElement.rows(i).cells(j).children.length;k++){
-                if(fTableElement.rows(i).cells(j).children[k].type == undefined){
-                    excelstr += fTableElement.rows(i).cells(j).children[k].innerText;
+            for(var k = 0; k < $("#exportToExel").rows(i).cells(j).children.length;k++){
+                if($("#exportToExel").rows(i).cells(j).children[k].type == undefined){
+                    excelstr += $("#exportToExel").rows(i).cells(j).children[k].innerText;
                 }else{
-                    excelstr += fTableElement.rows(i).cells(j).children[k].value;
+                    excelstr += $("#exportToExel").rows(i).cells(j).children[k].value;
                 }
             }
             excelstr += "</td>";
